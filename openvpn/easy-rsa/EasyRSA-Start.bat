e2e356f18842125c9580c8d21e1bdd6db45fe59b
JuanSystems <94084283+JuanSystems@users.noreply.github.com>
2024-12-26 19:50:33 -0500

    actualizacion

--- openvpn/easy-rsa/EasyRSA-Start.bat
+++
@@ -1,4 +0,0 @@
-@echo OFF
-rem Automatically set PATH to openssl.exe
-FOR /F "tokens=2*" %%a IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\OpenVPN" /v bin_dir') DO set "PATH=%PATH%;%%b"
-bin\sh.exe bin\easyrsa-shell-init.sh
